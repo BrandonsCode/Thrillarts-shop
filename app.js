14ba90527b6d2613358507cef97bb065016eee88
BrandonsCode <brandonscpucode@gmail.com>
2023-12-23 17:49:49 -0500

    remove tattoo photos

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv/config");
-const authJwt = require("./helpers/jwt");
-const errorHandler = require("./helpers/error-handler");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
 
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors())
 
 //middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(authJwt());
-app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
 //Routes
-const categoriesRoutes = require("./routes/categories");
-const productsRoutes = require("./routes/products");
-const usersRoutes = require("./routes/users");
-const ordersRoutes = require("./routes/orders");
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
 
