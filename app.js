0f6165e0a3b2702cec3946bd9a52b14c6cdbe026
BrandonsCode <brandonscpucode@gmail.com>
2023-12-27 01:29:59 -0500

    change  app.js back to what i had

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
-const errorHandler = require('./helper/error-handler');
-const authJwt = require('./helper/jwt');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+
 app.use(cors());
-app.options('*', cors);
- 
+app.options('*', cors())
+
 //middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
-app.use(errorHandler);
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
- 
+app.use(errorHandler);
+
 //Routes
 const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
 const ordersRoutes = require('./routes/orders');
- 
+
 const api = process.env.API_URL;
- 
-app.use(`${api}/products`, productsRoutes);
+
 app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
- 
-// Connecting to database
-mongoose
-  .connect(process.env.CONNECTION_STRING, {
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'eshop-database',
-  })
-  .then(() => {
-    console.log('Database connection is ready!');
-  })
-  .catch((err) => {
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
     console.log(err);
-  });
- 
-// app.listen(3000, () => {
-//   console.log(api);
-//   console.log('server is runninng at http://localhost:3000');
-// });
- 
-// Production
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    'Express server listening on port %d in %s mode',
-    this.address().port,
-    app.settings.env
-  );
-});
+})
+
+//Server
+// app.listen(3000, ()=>{
+
+//     console.log('server is running http://10.1.10.21:3000');
+// })
+
+//Production
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port)
+})
